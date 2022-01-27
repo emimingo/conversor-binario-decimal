8d8335ba334aa7a9aa12373f91ccbeb89de24cb3
emimingo <emilianomingo@gmail.com>
2022-01-27 18:15:55 -0300

    Iniciando app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+let input = document.querySelector("#num-binario");
+let btnConvertir = document.querySelector("#btn-convertir");
+let mostrar = document.querySelector(".mostrar");
+
+btnConvertir.addEventListener("click", (evento) => {
+    let binario = input.value;
+    //TODO: CREAR FUNCION DE VALIDACION
+
+    let resultado = convertirABinario(binario);
+    
+});
+
+function convertirABinario(binario){
+    alert(binario);
+}
